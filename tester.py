c68d9b2c76194da23be4574ee39b808ff76d4b82
Adam Kariv <adam.kariv@gmail.com>
2019-11-13 10:28:48 +0200

    Fix styles to be more responsive

---
+++ tester.py
@@ -0,0 +1,23 @@
+import requests
+import urllib.parse
+
+BODY = dict(
+    sections=[dict(
+        header='זהו מייל נסיון',
+        subheader='נעם, בבקשה שלח לי כמה סקרינשוטים!',
+        terms=[dict(
+            term='התאמה',
+            query_url='https://next.obudget.org/s/?q=%s&dd=entities' % urllib.parse.quote_plus('התאמה')
+        ), dict(
+            term='מסכים',
+            query_url='https://next.obudget.org/s/?q=%s&dd=procurement' % urllib.parse.quote_plus('מסכים')
+        ), dict(
+            term='שונים',
+            query_url='https://next.obudget.org/s/?q=%s&dd=budget' % urllib.parse.quote_plus('שונים')
+        )]
+    )],
+    email='noamoss@gmail.com'
+)
+
+if __name__ == '__main__':
+    print(requests.post('http://localhost:8000', json=BODY).content)
